29fbadfaa7e33ef858a47be2355222fe7770a7a7
Benjamin Muminovic <145535136+bmuminovic@users.noreply.github.com>
2023-11-03 09:40:22 +0100

    Update ex9.cpp

--- ex9.cpp
+++ ex9.cpp
@@ -8,15 +8,31 @@ using namespace std;
 // de fois que la caractère c apparait dans le fichier. Il doit retourner
 // une valeur négative en cas d'impossibilité d'ouvrir le fichier
 
+int nb_char_in_file(string filename, char c) {
+    ifstream file =  ifstream(filename);
+    if(!file) {
+        return -1;
+    }
+
+    char a;
+    int r = 0;
+    while(file >> noskipws >> a) {
+        if(a == c)  {
+            r++;
+        }
+    }
+    return r;
+}
+
 int main() {
    cout << "Entrez le nom du fichier : " << flush;
    string filename; cin >> filename;
-   cout << "Entrez le caractère : " << flush;
+   cout << "Entrez le caractere : " << flush;
    char c; cin >> c;
 
    auto n = nb_char_in_file(filename,c);
    if(n >= 0)
-      cout << "Le fichier contient " << n << " fois le caractère " << c << endl;
+      cout << "Le fichier contient " << n << " fois le caractere " << c << endl;
    else
       cout << "Le fichier est illisible";
 }
