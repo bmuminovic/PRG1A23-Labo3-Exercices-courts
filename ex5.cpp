94d52a23e89b6c031e3884e3b08c2348e7907faa
Benjamin Muminovic <145535136+bmuminovic@users.noreply.github.com>
2023-11-03 09:02:34 +0100

    Update ex5.cpp

--- ex5.cpp
+++ ex5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -6,26 +7,23 @@ using namespace std;
 // mots clés switch, break, continue et goto
 
 int main() {
-
    while(true) {
       int n;
       cin >> n;
       cin.ignore(numeric_limits<streamsize>::max(),'\n');
 
-      switch (n) {
-         case 1 :
-            cout << "1";
-         case 3 :
-            cout << "3";
-            break;
-         case 2 :
-            cout << "2";
-            continue;
-         case 4 :
-            cout << "4";
-            return 0;
-         default:
-            cout << "D";
+      if(n == 1) {
+          cout << "1";
+          cout << "3";
+      }else if(n == 3) {
+          cout << "3";
+      } else if(n == 2) {
+          cout << "2";
+      } else if (n == 4) {
+          cout << "4";
+          return 0;
+      } else {
+          cout << "D";
       }
       cout << endl;
    }
