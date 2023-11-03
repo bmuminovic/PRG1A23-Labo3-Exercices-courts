fdc736e0785cec30a186c45ffc43d989755117a9
Benjamin Muminovic <145535136+bmuminovic@users.noreply.github.com>
2023-11-03 09:02:10 +0100

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+
 using namespace std;
 
 // écrire la fonction somme_modulo_mille_milliards
 // qui retourne la somme de ses deux paramètres modulo
 // 1'000'000'000'000
-//
+
+// DONE
+
+long long somme_modulo_mille_milliards(long long mod1, long long mod2) {
+    return (mod1 + mod2) % 1'000'000'000'000;
+}
 
 int main() {
    cout << somme_modulo_mille_milliards(2,-3) << endl;
